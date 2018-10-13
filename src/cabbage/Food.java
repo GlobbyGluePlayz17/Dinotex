66faf20a1e6b1e56aae8d5b735d1754094be30fa
GlobbyGluePlayz17 <league@iMac-2.attlocal.net>
2018-10-13 15:59:24 -0700

    dinotex ip
    XP

--- src/cabbage/Food.java
+++ src/cabbage/Food.java
@@ -9,13 +9,6 @@ public class Food extends GameObject {
 	int xx = 5;
 	int yy = 7;
 	ArrayList<Food> listFood;
-	int xx1 = new Random().nextInt(100);
-	int xxx = xx*5;
-	int yy1 = new Random().nextInt(80);
-	int yyy = yy*10;
-	int weed = new Random().nextInt(35);
-	int hit = new Random().nextInt(35);
-	int numofbarir = new Random().nextInt(10);
 	
 	Food(int x, int y, int width, int height) {
 		super(x, y, width, height);
